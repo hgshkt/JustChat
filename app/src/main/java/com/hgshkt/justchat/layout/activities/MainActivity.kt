b22ff46cf80cdd47cd3b8810175f00bcd6e75848
hgshkt <nichtojestvozxc@gmail.com>
2023-03-18 06:35:04 +0300

    compose navigation drawer with screens samples

--- app/src/main/java/com/hgshkt/justchat/layout/activities/MainActivity.kt
+++ app/src/main/java/com/hgshkt/justchat/layout/activities/MainActivity.kt
@@ -1,57 +1,86 @@
 package com.hgshkt.justchat.layout.activities
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationView
-import com.hgshkt.justchat.R
-import com.hgshkt.justchat.auth.AppAuth
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.compose.rememberNavController
+import com.hgshkt.justchat.ui.AppBar
+import com.hgshkt.justchat.ui.DrawerBody
+import com.hgshkt.justchat.ui.MenuItem
+import com.hgshkt.justchat.ui.navigation.Navigation
+import com.hgshkt.justchat.ui.navigation.Screen
+import com.hgshkt.justchat.ui.theme.NavigationDrawerComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var navigationView: NavigationView
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        init()
-
-        val auth = AppAuth()
-
-        if (auth.entered) {
-            navigationView.setupWithNavController(navController)
-            appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
-
-            setupActionBarWithNavController(navController, appBarConfiguration)
-        } else {
-            Intent(applicationContext, LoginActivity::class.java).also {
-                startActivity(it)
+        setContent {
+            NavigationDrawerComposeTheme {
+                val navController = rememberNavController()
+                val scaffoldState = rememberScaffoldState()
+                val scope = rememberCoroutineScope()
+                Scaffold(
+                    scaffoldState = scaffoldState,
+                    topBar = {
+                        AppBar(
+                            onNavigationIconClick = {
+                                scope.launch {
+                                    scaffoldState.drawerState.open()
+                                }
+                            }
+                        )
+                    },
+                    drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
+                    drawerContent = {
+                        DrawerBody(
+                            items = listOf(
+                                MenuItem(
+                                    id = Screen.ChatListScreen.route,
+                                    title = "Chats",
+                                    contentDescription = "Go to chat list screen",
+                                    icon = Icons.Default.Email
+                                ),
+                                MenuItem(
+                                    id = Screen.CreatingChatScreen.route,
+                                    title = "Create chat",
+                                    contentDescription = "Go to creating chat screen",
+                                    icon = Icons.Default.Add
+                                ),
+                                MenuItem(
+                                    id = Screen.FriendListScreen.route,
+                                    title = "Friends",
+                                    contentDescription = "Go to friend list screen",
+                                    icon = Icons.Default.Person
+                                ),
+                                MenuItem(
+                                    id = Screen.ProfileScreen.route,
+                                    title = "Profile",
+                                    contentDescription = "Go to profile screen",
+                                    icon = Icons.Default.AccountBox
+                                )
+                            ),
+                            onItemClick = {
+                                println("Clicked on ${it.title}")
+                                navController.navigate(it.id)
+                                scope.launch {
+                                    scaffoldState.drawerState.close()
+                                }
+                            }
+                        )
+                    }
+                ) {
+                    Navigation(navController = navController)
+                }
             }
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    private fun init() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-        navController = navHostFragment!!.findNavController()
-        drawerLayout = findViewById(R.id.drawer_layout)
-        navigationView = findViewById(R.id.navigation_view)
     }
 }
